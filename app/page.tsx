4131977a9d943ffd4fac4aa782ada49852239a0c
LoTwT <709937065@qq.com>
2023-08-03 17:01:21 +0800

    feat: add dockbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef } from "react"
 import { Leafer } from "leafer-ui"
 import type { Nullable } from "@ayingott/sucrose"
 import { leaferAtom } from "./atoms"
+import DockBar from "@/components/DockBar"
 
 export default function Home() {
   const leaferRef = useRef<Nullable<HTMLDivElement>>(null)
@@ -18,8 +19,14 @@ export default function Home() {
   }, [leaferRef?.current])
 
   return (
-    <main className="w-full h-full">
+    <main className="w-full h-full relative">
       <div className="w-full h-full" ref={leaferRef} />
+      <DockBar
+        dockWrapper={{
+          direction: "horizontal",
+          position: "bottom",
+        }}
+      />
     </main>
   )
 }
---
+++ env.d.ts
@@ -0,0 +1,12 @@
+type CustomElement<T> = Partial<
+  T & { children: unknown } & import("react").DOMAttributes<T> &
+    import("react").HTMLAttributes<T> &
+    import("react").Attributes
+>
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    ["dock-wrapper"]: CustomElement<import("dockbar").DockWrapper>
+    ["dock-item"]: CustomElement<import("dockbar").DockItem>
+  }
+}
